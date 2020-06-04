af4417bb9e2da3277087264617ef79a90cda7f68
Death <xiaoxiao921@hotmail.fr>
2020-06-04 08:28:30 +0200

    Cook component in own file

---
+++ CHEF/Components/Commands/Cooking/Cook.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using Discord.Commands;
+using Discord.WebSocket;
+using Microsoft.EntityFrameworkCore;
+
+namespace CHEF.Components.Commands.Cooking
+{
+    public class Cook : Component
+    {
+        public Cook(DiscordSocketClient client) : base(client)
+        {
+
+        }
+
+        public override async Task SetupAsync()
+        {
+            using (var context = new RecipeContext())
+            {
+                await context.Database.MigrateAsync();
+                Logger.Log("Done migrating db.");
+            }
+
+            Client.MessageReceived += RecipeShortcutAsync;
+        }
+
+        private async Task RecipeShortcutAsync(SocketMessage msg)
+        {
+            if (!(msg is SocketUserMessage message))
+                return;
+
+            var argPos = 0;
+            if (!(message.HasCharPrefix('!', ref argPos) ||
+                  message.HasMentionPrefix(Client.CurrentUser, ref argPos)) ||
+                message.Author.IsBot)
+                return;
+
+            var cmdName = message.Content.Substring(1);
+            
+            using (var context = new RecipeContext())
+            {
+                var recipe = await context.Recipes.AsAsyncEnumerable()
+                    .FirstOrDefaultAsync(r => r.Name.ToLower().Equals(cmdName.ToLower()));
+                if (recipe != null)
+                {
+                    await msg.Channel.SendMessageAsync(
+                        $"**Recipe: {recipe.Name} (Owner: {recipe.OwnerName})**{Environment.NewLine}{recipe.Text}");
+                }
+            }
+        }
+    }
+}
