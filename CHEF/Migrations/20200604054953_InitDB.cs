cef607df82d438ceb8bf14f492382fc2e43133ca
Death <xiaoxiao921@hotmail.fr>
2020-06-04 08:28:08 +0200

    Migration for creating the DB

---
+++ CHEF/Migrations/20200604054953_InitDB.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+namespace CHEF.Migrations
+{
+    public partial class InitDB : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "recipes",
+                columns: table => new
+                {
+                    id = table.Column<int>(nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    owner_id = table.Column<decimal>(nullable: false),
+                    owner_name = table.Column<string>(nullable: true),
+                    name = table.Column<string>(nullable: true),
+                    text = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("pk_recipes", x => x.id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "recipes");
+        }
+    }
+}
