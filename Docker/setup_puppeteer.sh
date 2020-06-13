db67e528c232ceb14b74f179b08415e97ebc2c5a
Death <xiaoxiao921@hotmail.fr>
2020-06-13 03:27:01 +0200

    Remove useless fonts download for headless chrome

--- Docker/setup_puppeteer.sh
+++ Docker/setup_puppeteer.sh
@@ -5,8 +5,8 @@ apt-get update && apt-get -f install && apt-get -y install wget gnupg2 apt-utils
 wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | apt-key add - \
     && sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list' \
     && apt-get update \
-    && apt-get install -y google-chrome-stable fonts-ipafont-gothic fonts-wqy-zenhei fonts-thai-tlwg fonts-kacst ttf-freefont \
-      --no-install-recommends \
+    && apt-get install -y google-chrome-stable \
+	--no-install-recommends \
     && rm -rf /var/lib/apt/lists/*
  
 groupadd -r pptruser && useradd -r -g pptruser -G audio,video pptruser \
