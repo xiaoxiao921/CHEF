6d49c2038862ee2f055402b94ab49a97b65d0308
Death <xiaoxiao921@hotmail.fr>
2020-06-18 02:42:46 +0200

    Decode the env variable holding the google service credentials back into json

--- CHEF/Components/CloudVisionOcr.cs
+++ CHEF/Components/CloudVisionOcr.cs
@@ -1,4 +1,6 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.Text;
+using System.Threading.Tasks;
 using Discord.WebSocket;
 using Google.Cloud.Vision.V1;
 
@@ -14,7 +16,15 @@ namespace CHEF.Components
 
         public override async Task SetupAsync()
         {
-            AnnotatorClient = await ImageAnnotatorClient.CreateAsync();
+            byte[] data = Convert.FromBase64String(Environment.GetEnvironmentVariable("GOOGLE_SERVICE_CREDENTIALS_B64",
+                EnvironmentVariableTarget.Process));
+
+            var builder = new ImageAnnotatorClientBuilder
+            {
+                CredentialsPath = null,
+                JsonCredentials = Encoding.UTF8.GetString(data)
+            };
+            AnnotatorClient = await builder.BuildAsync();
         }
     }
 }
