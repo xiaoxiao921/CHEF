8e9c602e9705e571d4f2046607cf710d56ea2236
Death <xiaoxiao921@hotmail.fr>
2020-07-29 23:25:02 +0200

    Ignore Table Migration

---
+++ CHEF/Migrations/Ignore/20200729211740_IgnoreMigration.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CHEF.Migrations.Ignore
+{
+    public partial class IgnoreMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "user_ids",
+                columns: table => new
+                {
+                    discord_id = table.Column<decimal>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("pk_user_ids", x => x.discord_id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "user_ids");
+        }
+    }
+}
