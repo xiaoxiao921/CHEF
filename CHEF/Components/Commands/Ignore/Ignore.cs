2acf134e91fbec37786fb630453275526c5fcdfb
Death <xiaoxiao921@hotmail.fr>
2020-07-29 23:22:16 +0200

    Merge the table and its context on the same file

--- CHEF/Components/Commands/Ignore/Ignore.cs
+++ CHEF/Components/Commands/Ignore/Ignore.cs
@@ -1,21 +1,52 @@
-﻿using Discord.WebSocket;
+﻿using System.ComponentModel.DataAnnotations;
+using System.Threading.Tasks;
+using Discord.WebSocket;
+using Microsoft.EntityFrameworkCore;
 
 namespace CHEF.Components.Commands.Ignore
 {
-    class Ignore
+    public class IgnoreContext : DbContext
     {
-        public ulong discordId;
+        public DbSet<Ignore> UserIds { get; set; }
 
-        public override bool Equals(object obj)
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            if(obj is string @string)
+            // Dummy connection string for creating migration
+            // through the Package Manager Console with Add-Migration or dotnet ef
+            //optionsBuilder.UseNpgsql("Host=dummy;Username=dummy;Password=dummy;Database=dummy");
+
+            optionsBuilder.UseNpgsql(global::CHEF.Database.Connection, builder =>
             {
-                return discordId.ToString() == @string;
-            }if(obj is SocketUser user)
-            {
-                return discordId == user.Id;
-            }
-            return base.Equals(obj);
+#if RELEASE
+                // callback for validating the server certificate against a CA certificate file.
+                builder.RemoteCertificateValidationCallback(global::CHEF.Database.RemoteCertificateValidationCallback);
+#endif
+            });
+            optionsBuilder.UseSnakeCaseNamingConvention();
+        }
+
+        public async Task<Ignore> GetIgnore(ulong discordId) => 
+            await UserIds.AsQueryable()
+                .FirstOrDefaultAsync(i => i.Equals(discordId));
+
+
+        public async Task<bool> IsIgnored(SocketUser user) =>
+            await UserIds.AsQueryable().AnyAsync(i => i.Equals(user));
+    }
+
+    public class Ignore
+    {
+        [Key]
+        public ulong DiscordId { get; set; }
+
+        public bool Equals(ulong discordId)
+        {
+            return DiscordId == discordId;
+        }
+
+        public bool Equals(SocketUser user)
+        {
+            return DiscordId == user.Id;
         }
     }
 }
