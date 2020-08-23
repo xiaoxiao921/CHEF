ea061a1de0a99bbdbab474bf15dab03cdebbd4f5
Death <xiaoxiao921@hotmail.fr>
2020-08-23 19:26:35 +0200

    New command for giving nsfw role

---
+++ CHEF/Components/Commands/GiveRole.cs
@@ -0,0 +1,41 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+using Discord;
+using Discord.Commands;
+
+namespace CHEF.Components.Commands
+{
+    public class GiveRoleModule : ModuleBase<SocketCommandContext>
+    {
+        [Command("role")]
+        [Summary
+            ("Give / Remove the role given as first parameter to the user who called this command.")]
+        public async Task GiveRole(
+            [Summary("The role you want to give / remove access to.")]
+            string roleName)
+        {
+            if (roleName == "nsfw")
+            {
+                var user = Context.User;
+                var nsfwRole = Context.Guild.Roles.FirstOrDefault(x => x.Name == roleName);
+                var nsfwRoleId = nsfwRole?.Id;
+
+                if (user is IGuildUser gUser && nsfwRoleId != null)
+                {
+                    if (gUser.RoleIds.Any(id => id == nsfwRoleId))
+                    {
+                        await gUser.RemoveRoleAsync(nsfwRole);
+                    }
+                    else
+                    {
+                        await gUser.AddRoleAsync(nsfwRole);
+                    }
+                }
+            }
+            else
+            {
+                await Context.Channel.SendMessageAsync("Can only give/remove nsfw role for now.");
+            }
+        }
+    }
+}
