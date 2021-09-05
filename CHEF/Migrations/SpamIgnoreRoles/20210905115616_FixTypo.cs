af602253d6ac86d802cb244b2df98bd8aaa4ea47
xiaoxiao921 <xiaoxiao921@hotmail.fr>
2021-09-05 14:07:44 +0200

    fix include_message_content_in_log typo in db

---
+++ CHEF/Migrations/SpamIgnoreRoles/20210905115616_FixTypo.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CHEF.Migrations.SpamIgnoreRoles
+{
+    public partial class FixTypo : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn("inlcude_message_content_in_log", "spam_filter_configs", "include_message_content_in_log");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn("include_message_content_in_log", "spam_filter_configs", "inlcude_message_content_in_log");
+        }
+    }
+}
